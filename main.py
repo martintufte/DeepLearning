7cbc922373398f45e20fbb760a2555fe6a7138a8
Martin Gudahl Tufte <martingudahltufte@hotmail.com>
2022-01-31 12:35:56 +0100

    Add main file.

---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 12:16:39 2022
+
+@author: martigtu
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# First test
+class Dense:
+    def __init__(self):
+        self.n_neurons = 10
+        self.activation = "ReLU"
+        self.initialization = "Random"
+
+
+
+
+
+
+if __name__=="__main__":
+    print("It works!")
+    
+    L = Dense()
+    print("L has", L.n_neurons, "number of neurons!")
