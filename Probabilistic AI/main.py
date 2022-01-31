b97c8b992b4bf366727dcb58b6b8846ae9e6e5b8
Martin Gudahl Tufte <martingudahltufte@hotmail.com>
2022-01-31 12:44:50 +0100

    Add main files for project 2 and 3.

---
+++ Probabilistic AI/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 12:42:29 2022
+
+@author: martigtu
+"""
+
+# main file for Probabilistic AI
+
+if __name__=="__main__":
+    print("Have a good day!")
---
+++ Time Series prediction/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 12:43:50 2022
+
+@author: martigtu
+"""
+
+# main file for Time Series prediction
+
+if __name__=="__main__":
+    print("Have a good day!")
